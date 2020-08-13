d7790a1cd5b0f28ea9b3c00eb6533b855defd02c
WessCoby <cobygiven@gmail.com>
2020-08-13 11:42:24 +0000

    Initial Commit: Frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { 
+  BrowserRouter as Router, Switch, Redirect, Route,
+} from 'react-router-dom';
+import { ReactQueryDevtools as RQD } from 'react-query-devtools';
+
+import { NewPlace, UserPlaces, Users } from './pages';
+import { MainNavigation } from './components';
+
+
+const App = () => {
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact component={Users} />
+          <Route path="/:uid/places" exact component={UserPlaces} />
+          <Route path="/places/new" exact component={NewPlace} />
+          <Redirect to="/" />
+        </Switch>
+      </main>
+      <RQD initialIsOpen={false}/>
+    </Router>
+  );
+}
+
+export default App;
