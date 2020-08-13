c1b82f348415417edcb0f5fd85715e7293f2b8b0
WessCoby <cobygiven@gmail.com>
2020-08-13 11:58:32 +0000

    Setup Mock API with MirageJs

---
+++ frontend/src/mock-api/index.ts
@@ -0,0 +1,54 @@
+import { 
+  createServer, Model, hasMany, belongsTo, 
+  RestSerializer as RS
+} from 'miragejs';
+
+import seeds from './seeds'
+
+
+export default function () {
+  createServer({
+    models: {
+      user: Model.extend({
+        places: hasMany()
+      }),
+      place: Model.extend({
+        user: belongsTo()
+      })
+    },
+
+    serializers: {
+      user: RS.extend({
+        include: ['places'],
+        embed: true
+      }),
+      place: RS.extend({
+        include: ['user'],
+        embed: true
+      })
+    },
+
+    seeds,
+
+    routes() {
+      this.namespace = '/api';
+
+      this.get("/places", (schema: any) => schema.places.all());
+
+      this.post('/places', (schema: any, request) => {
+        let data = JSON.parse(request.requestBody);
+
+        return schema.places.create(data);
+      });
+
+      this.get('/places/user/:uid', (schema: any, request) => {
+        const uid = request.params.uid;
+        let user = schema.users.find(uid);
+
+        return { places: user ? user.places.models : [] };
+      });
+
+      this.get('/users', (schema: any) => schema.users.all())
+    },
+  })
+}
