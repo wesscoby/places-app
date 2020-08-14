f0b287f64f385ef9d423156565426fcd656796a9
WessCoby <cobygiven@gmail.com>
2020-08-14 20:14:14 +0000

    Added export for updatePlace Schema

--- frontend/src/util/validation/place.ts
+++ frontend/src/util/validation/place.ts
@@ -6,3 +6,7 @@ import { title, description, address } from './props';
 export const newPlaceSchema = Obj().shape({
   title, description, address
 });
+
+export const updatePlaceSchema = Obj().shape({
+  title, description
+});
