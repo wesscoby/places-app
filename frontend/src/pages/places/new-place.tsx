3e9662c454d4fa7300afd18138880f9c6390c02c
WessCoby <cobygiven@gmail.com>
2020-08-14 00:03:05 +0000

    Updated NewPlace page: Setup form

--- frontend/src/pages/places/new-place.tsx
+++ frontend/src/pages/places/new-place.tsx
@@ -1,11 +1,72 @@
 import React, { FC } from 'react';
+import { Formik, Form } from 'formik';
+
+import { Input, Button } from '../../components';
+import { newPlaceSchema } from '../../util';
 
 
-const NewPlace: FC<any> = () => {
+interface NewPlaceSchema {
+  title: string;
+  description: string;
+  address: string;
+}
+
+const NewPlace: FC = () => {
+  const schemaValues: NewPlaceSchema = {
+    title: '',
+    description: '',
+    address: ''
+  };
+  
   return (
-    <form className="place-form">
-      <p>Form...</p>
-    </form>
+    <div className="place-form">
+      <Formik
+        initialValues={schemaValues}
+        validationSchema={newPlaceSchema}
+        onSubmit={(
+          { title, description, address }: NewPlaceSchema, 
+          { setSubmitting }
+        ) => {
+          console.log({ title, description, address })
+          setSubmitting(true);
+          setTimeout(() => {
+            setSubmitting(false);
+          }, 3000);
+        }}
+      >
+        {({ 
+          isSubmitting, errors, handleChange, 
+          handleBlur, values: { title }, isValid
+        }) => (
+          <Form>
+            <Input
+              label="Title"
+              name="title"
+              placeholder="Enter title"
+              errorText={errors.title}
+            />
+            <Input
+              textarea
+              label="Description"
+              name="description"
+              placeholder={`Provide some information about ${title ? title : 'this place'}`}
+              errorText={errors.description}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <Input
+              label="Address"
+              name="address"
+              placeholder="Enter a valid address"
+              errorText={errors.address}
+            />
+            <Button type="submit" disabled={isSubmitting || !isValid}>
+              ADD PLACE
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
 
