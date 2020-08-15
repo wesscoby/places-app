7a484ce60fdafe90d68d3e9c6751cde6c01d266a
WessCoby <cobygiven@gmail.com>
2020-08-15 23:37:31 +0000

    Setup react-query global config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,25 +3,34 @@ import {
   BrowserRouter as Router, Switch, Redirect, Route,
 } from 'react-router-dom';
 import { ReactQueryDevtools as RQD } from 'react-query-devtools';
+import { 
+  ReactQueryConfigProvider, ReactQueryProviderConfig 
+} from 'react-query';
 
 import { NewPlace, UserPlaces, Users, UpdatePlace } from './pages';
 import { MainNavigation } from './components';
 
 
+const queryConfig: ReactQueryProviderConfig = {
+  queries: { refetchOnWindowFocus: false }
+}
+
 const App = () => {
   return (
     <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path="/" exact component={Users} />
-          <Route path="/:uid/places" exact component={UserPlaces} />
-          <Route path="/places/new" exact component={NewPlace} />
-          <Route path="/places/:pid" component={UpdatePlace} />
-          <Redirect to="/" />
-        </Switch>
-      </main>
-      <RQD initialIsOpen={false}/>
+      <ReactQueryConfigProvider config={queryConfig}>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route path="/" exact component={Users} />
+            <Route path="/:uid/places" exact component={UserPlaces} />
+            <Route path="/places/new" exact component={NewPlace} />
+            <Route path="/places/:pid" component={UpdatePlace} />
+            <Redirect to="/" />
+          </Switch>
+        </main>
+        <RQD initialIsOpen={false}/>
+      </ReactQueryConfigProvider>
     </Router>
   );
 }
