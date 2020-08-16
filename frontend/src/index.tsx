806f5402aebabe06e88a68c4cf499b5703e9255a
WessCoby <cobygiven@gmail.com>
2020-08-16 22:26:40 +0000

    Added dev environment conditional

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom';
 
 import './scss/index.scss';
 import App from './App';
+import { AuthContextProvider } from './context';
 import * as serviceWorker from './serviceWorker';
 import mockAPI from './mock-api';
 
-// Start Mock API
-mockAPI();
+// Start Mock API in development
+if(process.env.NODE_ENV === 'development') {
+  mockAPI({ environment: 'development' });
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
