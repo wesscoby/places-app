d2f8fe3f603633282a1e65617a368fc3d627fce4
WessCoby <cobygiven@gmail.com>
2020-08-19 16:20:58 +0000

    Setup `shared` module

---
+++ backend/src/shared/index.ts
@@ -0,0 +1,6 @@
+export * from './config';
+export * from './shared.module';
+export * from './base.model';
+export * from './base.model.dto';
+export * from './filters';
+export * from './base.service';
