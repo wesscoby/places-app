fc6b7d4c7d0cbb16bf9641c7173011d2445f433b
WessCoby <cobygiven@gmail.com>
2020-08-19 18:44:02 +0000

    Setup UserModule

---
+++ backend/src/user/index.ts
@@ -0,0 +1,6 @@
+export * from './user.controller';
+export * from './user.dto';
+export * from './user.helper';
+export * from './user.model';
+export * from './user.service';
+export * from './user.module';
