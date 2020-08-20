94fa0e4a9d86c11d873aaf6be6ff154c41a5f72f
WessCoby <cobygiven@gmail.com>
2020-08-20 01:23:45 +0000

    Added member mapper for `places`

--- backend/src/user/user.profile.ts
+++ backend/src/user/user.profile.ts
@@ -1,13 +1,20 @@
-import { AutoMapper, Profile, ProfileBase } from 'nestjsx-automapper';
+import { AutoMapper, Profile, ProfileBase, mapWith } from 'nestjsx-automapper';
 
 import { User } from './user.model';
 import { UserDTO } from './user.dto';
+import { PlaceDTO, Place } from "../places";
 
 
 @Profile()
 export class UserProfile extends ProfileBase {
   constructor(mapper: AutoMapper) {
     super();
-    mapper.createMap(User, UserDTO);
+    mapper
+    .createMap(User, UserDTO)
+    .forMember(destination => destination.places,
+      mapWith(PlaceDTO, source => source.places,
+        () => Place
+      )
+    );
   }
 }
