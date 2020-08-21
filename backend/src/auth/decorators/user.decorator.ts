573073c79d194c0b7a70bb26797445829e72bce2
WessCoby <cobygiven@gmail.com>
2020-08-21 17:38:02 +0000

    Moved user.decorator to AuthModule. Added auth decorator

--- backend/src/auth/decorators/user.decorator.ts
+++ backend/src/auth/decorators/user.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 
-export const User = createParamDecorator(
+export const ReqUser = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const user = request.user;
