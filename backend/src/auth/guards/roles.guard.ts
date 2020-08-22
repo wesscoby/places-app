9c4eba2a6948756ead28250dd1393b7d84081428
WessCoby <cobygiven@gmail.com>
2020-08-22 14:44:02 +0000

    Default to `user` when no role is passed to Auth decorator

--- backend/src/auth/guards/roles.guard.ts
+++ backend/src/auth/guards/roles.guard.ts
@@ -8,7 +8,7 @@ export class RolesGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     const roles = this.reflector.get<string[]>('roles', context.getHandler());
-    if (!roles) {
+    if (!roles || roles.length === 0) {
       return true;
     }
     const request = context.switchToHttp().getRequest();
