68d283d7f689aa0038a126c342bc500cdcff7e48
WessCoby <cobygiven@gmail.com>
2020-08-23 04:20:46 +0000

    Added `IsAdmin` decorator

--- backend/src/auth/decorators/user.decorator.ts
+++ backend/src/auth/decorators/user.decorator.ts
@@ -1,5 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+import { Role } from '../models';
+
 
 export const ReqUser = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
@@ -9,3 +11,11 @@ export const ReqUser = createParamDecorator(
     return data ? user && user[data] : user;
   },
 );
+
+export const IsAdmin = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const role = request.user.role;
+    return role === Role.ADMIN;
+  }
+)
