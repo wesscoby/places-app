18f5c9d1a9939b0958e2b2d38432a12e588f6100
WessCoby <cobygiven@gmail.com>
2020-08-23 06:22:13 +0000

    Added methods to fetch and update user profile

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import {
-  Controller, UseGuards, Post, UseInterceptors, Get, Body
+  Controller, UseGuards, Post, UseInterceptors, Get, Body, Patch
 } from '@nestjs/common';
-import { ApiTags, ApiBody } from '@nestjs/swagger';
+import { ApiTags, ApiBody, ApiOperation, ApiOkResponse } from '@nestjs/swagger';
 
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards';
 import { TransformInterceptor } from '../shared';
-import { LoginUserDto, CreateUserDto } from '../user';
+import { 
+  LoginUserDto, CreateUserDto, UserProfile, UpdateUserDto 
+} from '../user';
 import { ReqUser, Auth } from './decorators';
 
 
@@ -17,6 +19,7 @@ export class AuthController {
   constructor(private readonly auth: AuthService) {}
 
   @Post('signup')
+  @ApiOperation({ description: 'New User sign up' })
   @ApiBody({ type: () => CreateUserDto })
   async signup(@Body() user: CreateUserDto) {
     return await this.auth.signup(user);
@@ -24,14 +27,31 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
+  @ApiOperation({ description: 'User login' })
   @ApiBody({ type: () => LoginUserDto })
   async login(@ReqUser() user) {
     return this.auth.login(user);
   }
 
   @Auth()
+  @ApiOperation({ description: 'Get authenticated user profile' })
+  @ApiOkResponse({ type: () => UserProfile })
   @Get('profile')
-  getProfile(@ReqUser() user) {
-    return user;
+  async getProfile(@ReqUser('id') uid: string): Promise<UserProfile> {
+    return await this.auth.getUserProfile(uid);
+  }
+
+  @Auth()
+  @ApiOperation({ 
+    description: 'Update authenticated user info [name, avatar]' 
+  })
+  @ApiBody({ type: () => UpdateUserDto })
+  @ApiOkResponse({ type: () => UserProfile })
+  @Patch('profile')
+  async updateProfile(
+    @ReqUser('id') uid: string,
+    @Body() update: UpdateUserDto
+  ): Promise<UserProfile> {
+    return await this.auth.updateProfile(uid, update);
   }
 }
