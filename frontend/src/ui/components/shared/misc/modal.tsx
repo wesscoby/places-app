fa5f662433db6e79a021ebfb20d373756f6b9f1b
WessCoby <cobygiven@gmail.com>
2020-08-31 18:33:14 +0000

    Switched to using `react-modal`

--- frontend/src/ui/components/shared/misc/modal.tsx
+++ frontend/src/ui/components/shared/misc/modal.tsx
@@ -1,65 +1,50 @@
 import React, { FC, FormEvent } from 'react';
-import { createPortal } from 'react-dom';
-import { CSSTransition } from 'react-transition-group';
+import ReactModal from 'react-modal';
 
-import { Backdrop } from '..';
 import { OnClickEvent } from '../../../../util';
 
 
-interface ModalOverlayProps {
+interface ModalProps {
+  show: boolean;
+  onCancel?: OnClickEvent;
   style?: any;
   className?: string;
   headerClass: string;
   contentClass: string;
   footerClass: string;
   header: string;
-  footer: any;  // temp type
+  footer: any;
   onSubmit?: (e: FormEvent) => void
 }
 
-interface ModalProps extends ModalOverlayProps {
-  show: boolean;
-  onCancel?: OnClickEvent;
-}
-
-const ModalOveralay: FC<ModalOverlayProps> = ({
-  style = {}, className, headerClass, header, footer,
-  onSubmit, contentClass, footerClass, children
+const Modal: FC<ModalProps> = ({
+  onCancel, onSubmit, show, children, className, style={}, headerClass, header,
+  contentClass, footer, footerClass
 }) => {
-  const content = (
-    <div className={`modal ${className ? className : ''} `} style={style}>
-      <header className={`modal__header ${headerClass}`}>
-      <h2>{header}</h2>
-      </header>
-      <form onSubmit={onSubmit ? onSubmit : e => e.preventDefault()}>
-        <div className={`modal__content ${contentClass}`}>
-          {children}
-        </div>
-        <footer className={`modal__footer ${footerClass}`}>
-          {footer}
-        </footer>
-      </form>
-    </div>
-  );
-  const modalHook = document.getElementById('modal-hook')!;
+  ReactModal.setAppElement('#modal-hook');
 
-  return createPortal(content, modalHook);
-}
-
-
-const Modal: FC<ModalProps> = ({ 
-  show, onCancel, ...props
-}) => {
   return (
-    <>
-      {show && <Backdrop onClick={onCancel} />}
-      <CSSTransition 
-        in={show} mountOnEnter unmountOnExit
-        timeout={200} classNames="modal"
-      >
-        <ModalOveralay {...props} />
-      </CSSTransition>
-    </>
+    <ReactModal
+      isOpen={show}
+      className="modal"
+      overlayClassName="backdrop"
+      onRequestClose={onCancel}
+      contentLabel="Modal"
+    >
+      <div className={`modal ${className ? className : ''} `} style={style}>
+        <header className={`modal__header ${headerClass}`}>
+          <h2>{header}</h2>
+        </header>
+        <form onSubmit={onSubmit ? onSubmit : e => e.preventDefault()}>
+          <div className={`modal__content ${contentClass}`}>
+            {children}
+          </div>
+          <footer className={`modal__footer ${footerClass}`}>
+            {footer}
+          </footer>
+        </form>
+      </div>
+    </ReactModal>
   )
 }
 
