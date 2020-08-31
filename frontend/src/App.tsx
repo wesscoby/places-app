509a759f92cf1c8ba9887c7b11d425a72bdaccfb
WessCoby <cobygiven@gmail.com>
2020-08-31 18:35:51 +0000

    Added Google Maps LoadScript [#11]. Added config from `react-toastify`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,12 @@ import { ReactQueryDevtools as RQD } from 'react-query-devtools';
 import {
   ReactQueryConfigProvider, ReactQueryProviderConfig
 } from 'react-query';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { MainNavigation } from './ui/components';
 import { Routes } from './routes';
+import { LoadScript } from '@react-google-maps/api';
 
 
 const queryConfig: ReactQueryProviderConfig = {
@@ -16,16 +19,24 @@ const queryConfig: ReactQueryProviderConfig = {
   }
 }
 
+const libraries = ["places"];
+
+toast.configure();
 const App: FC = () => {
   return (
     <ReactQueryConfigProvider config={queryConfig}>
-      <Router>
-        <MainNavigation />
-        <main>
-          <Routes />
-        </main>
-        <RQD initialIsOpen={false}/>
-      </Router>
+      <LoadScript
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+        libraries={libraries}
+      >
+        <Router>
+          <MainNavigation />
+          <main>
+            <Routes />
+          </main>
+          <RQD initialIsOpen={false}/>
+        </Router>
+      </LoadScript>
     </ReactQueryConfigProvider>
   );
 }
