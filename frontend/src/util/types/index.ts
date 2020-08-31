7f70a5c4af4eb44d56abcdeab15cf91dd7f7ab91
WessCoby <cobygiven@gmail.com>
2020-08-31 19:30:09 +0000

    Added new types

--- frontend/src/util/types/index.ts
+++ frontend/src/util/types/index.ts
@@ -20,10 +20,52 @@ export interface User {
   id: string;
   name: string;
   email: string;
-  avatar: string;
+  avatar?: string;
   places: Place[];
 }
 
 export type OnClickEvent = (event: MouseEvent<HTMLElement>) => void;
 export type onChangeEvent = (event: ChangeEvent<HTMLElement>) => void;
 export type onBlurEvent = (event: FocusEvent<HTMLElement>) => void;
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface SignupData {
+  name: string;
+  email: string;
+  password: string
+}
+
+export enum Role {
+  USER = 'user',
+  ADMIN = 'admin'
+}
+
+export interface UserProfile {
+  id: string;
+  name: string;
+  email: string;
+  avatar?: string;
+  role: Role;
+}
+
+export interface AuthData {
+  user: UserProfile;
+  token: string;
+}
+
+export interface AddPlaceData {
+  title: string;
+  description: string;
+  address: string;
+  image?: string;
+  coordinates?: LatLng;
+}
+
+export interface UpdatePlaceData {
+  title?: string;
+  description?: string;
+}
