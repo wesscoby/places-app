d5068f4a68c2b3a90f92a62603ca30e7b42d0370
WessCoby <cobygiven@gmail.com>
2020-08-31 22:48:12 +0000

    Added spinner and Error components

--- frontend/src/ui/components/shared/misc/index.ts
+++ frontend/src/ui/components/shared/misc/index.ts
@@ -3,3 +3,5 @@ export { default as Card } from './card';
 export { default as Backdrop } from './backdrop';
 export { default as Modal } from './modal';
 export { default as Map } from './map';
+export { default as Spinner } from './spinner';
+export { default as Error } from './error';
