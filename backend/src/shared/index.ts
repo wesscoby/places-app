7b4769619014b3549fa2768b540e01574bf5eec9
WessCoby <cobygiven@gmail.com>
2020-09-06 08:59:55 +0000

    Added yup validation schemas [Validation #10]

--- backend/src/shared/index.ts
+++ backend/src/shared/index.ts
@@ -3,4 +3,6 @@ export * from './models';
 export * from './filters';
 export * from './services';
 export * from './interceptors';
-// export * from './decorators';
+export * from './pipes';
+export * from './validation';
+export * from './decorators';
