f9c3c95a4aef3cf15eacabd9b05ab077d9bfa699
WessCoby <cobygiven@gmail.com>
2020-09-06 08:56:59 +0000

    Added decorator for swagger api response

--- backend/src/shared/decorators/validate.decorator.ts
+++ backend/src/shared/decorators/validate.decorator.ts
@@ -1,10 +1,12 @@
 import { applyDecorators, UsePipes } from '@nestjs/common';
 import { Schema } from 'yup';
+import { ApiBadRequestResponse } from '@nestjs/swagger';
 
 import { YupValidationPipe } from '../pipes';
 
 export function Validate<T>(schema: Schema<T>) {
   return applyDecorators(
+    ApiBadRequestResponse({ description: 'Validation failed' }),
     UsePipes(new YupValidationPipe(schema))
   );
 }
