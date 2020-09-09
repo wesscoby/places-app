0abc625537d1e579a094384e1e3e75be4816a0cb
WessCoby <cobygiven@gmail.com>
2020-09-09 10:20:13 +0000

    Added `GravatarService`

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -3,6 +3,7 @@ import { TypegooseModule as TM } from 'nestjs-typegoose';
 
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { GravatarService } from './gravatar.service';
 import { UserModel } from './models';
 import './user.profile';
 import { AuthModule } from '../auth';
@@ -16,7 +17,7 @@ import { PlacesModule } from '../places';
     TM.forFeature([UserModel])
   ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, GravatarService],
   exports: [UserService]
 })
 export class UserModule {}
